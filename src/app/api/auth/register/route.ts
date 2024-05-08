df090f865efcf07b923f25705f54a357c8aa27cf
Emanuel Torres <emanueljtc90@gmai.com>
2024-05-08 18:25:13 -0400

    feat: create register page

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from 'next/server';
+import db from '@/libs/db';
+import bcrypt from 'bcrypt';
+
+export async function POST(request: { json: () => any }) {
+  try {
+    const data = await request.json();
+    const userFound = await db.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+    const usernameFound = await db.user.findUnique({
+      where: {
+        username: data.username,
+      },
+    });
+    if (userFound) {
+      return NextResponse.json(
+        { message: 'email already exists' },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (usernameFound) {
+      return NextResponse.json(
+        { message: 'username already exists' },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const newUser = await db.user.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...user } = newUser;
+
+    return NextResponse.json(user, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: error.message },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,149 @@
+'use client';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import { useRouter } from 'next/navigation';
+import './style.css';
+const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    criteriaMode: 'all',
+  });
+
+  const router = useRouter();
+  const onSubmit = handleSubmit(async (data) => {
+    if (data.password !== data.confirmPassword) {
+      return alert('password not match');
+    }
+    const res = await fetch('/api/auth/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (res.ok) {
+      router.push('/auth/login');
+    }
+  });
+
+  return (
+    <div className="content">
+      <form action="/api/auth/register" onSubmit={onSubmit} className="form">
+        <h1 className="title">Register</h1>
+        <label htmlFor="username" className="label">
+          Username:
+        </label>
+        <input
+          type="text"
+          {...register('username', {
+            required: 'this field is required',
+            maxLength: {
+              value: 10,
+              message: 'max length is 10',
+            },
+          })}
+          className="input"
+          placeholder="username"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="username"
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => {
+              return (
+                <span key={type} className="error-message">
+                  {message}
+                </span>
+              );
+            })
+          }
+        />
+        <label htmlFor="email" className="label">
+          Email:
+        </label>
+        <input
+          type="email"
+          {...register('email', { required: 'this field is required' })}
+          className="input"
+          placeholder="l5lXK@example.com"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="email"
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => {
+              return (
+                <span key={type} className="error-message">
+                  {message}
+                </span>
+              );
+            })
+          }
+        />
+        <label htmlFor="password" className="label">
+          Password:
+        </label>
+        <input
+          type="password"
+          {...register('password', { required: 'this field is required' })}
+          className="input"
+          placeholder="******"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="password"
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => {
+              return (
+                <span key={type} className="error-message">
+                  {message}
+                </span>
+              );
+            })
+          }
+        />
+
+        <label htmlFor="confirmPassword" className="label">
+          Confirm Password:
+        </label>
+        <input
+          type="password"
+          {...register('confirmPassword', { required: true })}
+          className="input"
+          placeholder="******"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="confirmPassword"
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => {
+              return (
+                <span key={type} className="error-message">
+                  {message}
+                </span>
+              );
+            })
+          }
+        />
+        <button type="submit" className="button-primary mt-4">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
+
