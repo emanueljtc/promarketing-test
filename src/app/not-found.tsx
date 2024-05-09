4d35d4dc82814b7f778381f80e136f57cd68c4fe
Emanuel Torres <emanueljtc90@gmai.com>
2024-05-09 02:52:48 -0400

    feat: add notFound page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function notFound() {
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-center p-24">
+      <span className="text-8xl mb-4 animate-shake animate-infinite">&#128558;</span>
+      <h1 className="text-8xl text-white">404</h1>
+      <h1 className="text-4xl text-white">Not Found</h1>
+    </section>
+  );
+}
+
